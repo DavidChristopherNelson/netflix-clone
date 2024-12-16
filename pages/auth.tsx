675c71c2ef2d94e7c18da233b20dc70ff184df38
David Nelson <david.nelson1@uqconnect.edu.au>
2024-12-16 10:47:22 +1000

    Create sign in fields and log in button.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import Input from "../components/input";
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -13,8 +17,30 @@ const Auth = () => {
               Sign In
             </h2>
             <div className="flex flex-col gap-4">
-              <Input />
+              <Input 
+                label="Username"
+                onChange={(event: any) => setName(event.target.value)}
+                id="name"
+                value={name}
+              />
+              <Input 
+                label="Email"
+                onChange={(event: any) => setEmail(event.target.value)}
+                id="email"
+                value={email}
+                type="email"
+              />
+              <Input 
+                label="Password"
+                onChange={(event: any) => setPassword(event.target.value)}
+                id="password"
+                value={password}
+                type="password"
+              />
             </div>
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              Login
+            </button>
           </div>
         </div>
       </div>
